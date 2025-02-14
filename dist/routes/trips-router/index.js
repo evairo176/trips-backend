4072021996bb394c1631f7b4f809af2f9aafe4ad
Dicki Prasetya <98743315+evairo176@users.noreply.github.com>
2025-02-14 18:55:58 +0700

    Delete dist directory

--- dist/routes/trips-router/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const middlewares_1 = require("../../middlewares");
-const tripsController_1 = require("../../controller/tripsController");
-const tripsRouter = (0, express_1.Router)();
-// register auth
-tripsRouter.get('/', middlewares_1.authenticateJWT, tripsController_1.getTripsController);
-exports.default = tripsRouter;
